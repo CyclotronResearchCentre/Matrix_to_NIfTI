6252f357c6016b1b574def5e9e2ad38233c05df4
Christophe Phillips <c.phillips@uliege.be>
2020-10-30 17:45:55 +0100

    Deal with series of 2D images + 3D image

--- cp_mat2nii.m
+++ cp_mat2nii.m
@@ -12,13 +12,10 @@ function fn_out = cp_mat2nii(M,varargin)
 %   M,   matrix to write out in NIfTI format iamge
 %   opt, option structure or list of keyword/values pairs
 %     .fn_nii, filename of the .nii file, def. 'image.nii'
+%     .pth_nii, provide path where to write the image(s), def. 'pwd'
 %     .force2D, force writing of only 2D images, even if the input is 3D,
 %               def. true
 %     .dtype, data type for .nii file, def. float32 (see spm_type)
-% 
-% Dependences:
-% This function relies on some SPM functions, mainly 'spm_vol.' and
-% 'spm_write_vol.m', thus SPM's main folder must be on Matlab's path.
 %__________________________________________________________________________
 % Copyright (C) 2020 Cyclotron Research Centre
 
@@ -34,6 +31,7 @@ end
 %--------------------------------------------------------------------------
 opts = struct( ...
     'fn_nii', 'image.nii', ... % filename
+    'pth_nii', pwd, ... % path to output folder
     'force2D', true, ...  % force 2D input
     'dtype', 16 ... % data type (float32)
     ) ;
@@ -58,16 +56,16 @@ for ii=1:numel(fn)
 end
 
 %% Deal with the matrix
-M_sz = size(M);
+Mo_sz = size(M);
 % Number of images to write out: N_nii
-switch numel(M_sz) 
+switch numel(Mo_sz) 
     case 2,
         N_nii = 1;
-        M_sz(3) = 1;
+        Mo_sz(3) = 1;
     case 3,
         if opts.force2D
-            N_nii = M_sz(3);
-            M_sz(3) = 1;
+            N_nii = Mo_sz(3);
+            Mo_sz(3) = 1;
         else
             N_nii = 1;
         end
@@ -75,15 +73,32 @@ switch numel(M_sz)
         error('mat2nii:M_size','Matrix must be 2D or 3D.');
 end
 
-% Create a single image
-V = struct( ...
-    'fname', opts.fn_nii, ...
-    'dim', M_sz, ...
-    'dt', [opts.dtype 0], ...
-    'mat', eye(4) );
-Vo = spm_write_vol(V,M);
+% create full filename(s)
+ffn = cell(N_nii,1);
+if ~exist(opts.pth_nii,'dir'), mkdir(opts.pth_nii); end
+if N_nii==1
+    ffn{1} = fullfile(opts.pth_nii,opts.fn_nii);
+else
+    for ii = 1:N_nii
+        ffn{ii} = fullfile(opts.pth_nii, ...
+            spm_file(opts.fn_nii,'suffix',sprintf('_%03d',ii)));
+    end
+end
+% Create the images image(s)
+for ii = 1:N_nii
+    V = struct( ...
+        'fname', ffn{ii}, ...
+        'dim', Mo_sz, ...
+        'dt', [opts.dtype 0], ...
+        'mat', eye(4) );
+    if Mo_sz(3)>1 && N_nii==1
+        Vo = spm_write_vol(V,M); % Single 3D image case
+    else
+        Vo = spm_write_vol(V,M(:,:,ii)); %#ok<*NASGU>
+    end
+end
 
 %% Output
-fn_out = Vo.fname;
+fn_out = char(ffn);
 
 end
